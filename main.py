c060037eb00297f8deede2a43a19e2a6c2adabdc
GTA-6 <bwangrui@gmail.com>
2021-05-28 16:45:09 -0400

    added a few arguments

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ model.load_forcings(site=site)
 
 fig, ax = plt.subplots(2,2)
 print('running the default model')
-model.run_selm(use_nn=True, seasonal_rootalloc=False, spinup_cycles=2)
+model.run_selm(use_nn=True, prefix='default', seasonal_rootalloc=False, spinup_cycles=2,nfroot_orders=1)
 ax[0,0].plot(model.output['lai_pft'].squeeze()[0:365],'b')
 ax[0,0].set_ylabel('LAI')
 ax[0,1].plot(model.output['leafc_pft'].squeeze()[0:365],'b')
@@ -26,7 +26,7 @@ ax[1,1].set_ylabel('Dead stem C')
 print('running model with fine root phenology')
 model.parms['froot_phen_width'][:]=0.6
 model.parms['froot_phen_peak'][:]=0.5
-model.run_selm(use_nn=True, seasonal_rootalloc=True, spinup_cycles=2)
+model.run_selm(use_nn=True, prefix='root_phen', seasonal_rootalloc=True, spinup_cycles=2, nfroot_orders=1)
 ax[0,0].plot(model.output['lai_pft'].squeeze()[0:365],'r')
 ax[0,1].plot(model.output['leafc_pft'].squeeze()[0:365],'r')
 ax[1,0].plot(model.output['frootc_pft'].squeeze()[0:365],'r')
