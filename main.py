3439e202c939f442a35219b844a49eb9d3cddbbe
leavesandlemmas <soswald77@gmail.com>
2022-02-25 14:39:15 -0500

    New branch

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 ##!/usr/bin/python
 import model_sELM as models
-import os, sys, math, random
-import forcings
-from utils import *
-import matplotlib.pyplot as plt
+# import os, sys, math, random
+# import forcings
+# from utils import *
+# import matplotlib.pyplot as plt
+from numpy import ndarray
+from json import dump
 
 site = 'US-Ho1'
 #create model object
@@ -11,29 +13,33 @@ model = models.MyModel(site=site)
 #Load model forcings
 model.load_forcings(site=site)
 
-fig, ax = plt.subplots(2,2)
+# fig, ax = plt.subplots(2,2)
 print('running the default model')
-model.run_selm(use_nn=True, seasonal_rootalloc=False, spinup_cycles=2)
-ax[0,0].plot(model.output['lai_pft'].squeeze()[0:365],'b')
-ax[0,0].set_ylabel('LAI')
-ax[0,1].plot(model.output['leafc_pft'].squeeze()[0:365],'b')
-ax[0,1].set_ylabel('Leaf C')
-ax[1,0].plot(model.output['frootc_pft'].squeeze()[0:365],'b')
-ax[1,0].set_ylabel('Froot C')
-ax[1,1].plot(model.output['deadstemc_pft'].squeeze()[0:365],'b')
-ax[1,1].set_ylabel('Dead stem C')
 
-print('running model with fine root phenology')
-model.parms['froot_phen_width'][:]=0.6
-model.parms['froot_phen_peak'][:]=0.5
-model.run_selm(use_nn=True, seasonal_rootalloc=True, spinup_cycles=2)
-ax[0,0].plot(model.output['lai_pft'].squeeze()[0:365],'r')
-ax[0,1].plot(model.output['leafc_pft'].squeeze()[0:365],'r')
-ax[1,0].plot(model.output['frootc_pft'].squeeze()[0:365],'r')
-ax[1,1].plot(model.output['deadstemc_pft'].squeeze()[0:365],'r')
-ax[1,1].legend(['Default','Root phenology'])
-
-plt.show()
+kwargs = dict(use_nn=True, seasonal_rootalloc=False, spinup_cycles=3)
+model.run_selm(**kwargs)
+# ax[0,0].plot(model.output['lai_pft'].squeeze()[0:365],'b')
+# ax[0,0].set_ylabel('LAI')
+# ax[0,1].plot(model.output['leafc_pft'].squeeze()[0:365],'b')
+# ax[0,1].set_ylabel('Leaf C')
+# ax[1,0].plot(model.output['frootc_pft'].squeeze()[0:365],'b')
+# ax[1,0].set_ylabel('Froot C')
+# ax[1,1].plot(model.output['deadstemc_pft'].squeeze()[0:365],'b')
+# ax[1,1].set_ylabel('Dead stem C')
 
 
-#print(i, rmse)
+
+data = {}
+for key in model.output:
+    datum = model.output[key]
+    if isinstance(datum, ndarray):
+        datum = datum.tolist()
+    
+    data[key] = datum
+
+# ADD site and kwargs
+data['site'] = site
+data['kwargs'] = kwargs
+
+with open('../simple_ELM-output/model_output_spinup_3.json', 'w') as jsonfile:
+    dump(data, jsonfile)
